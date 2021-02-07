e869fb9e5db392ba582dd5dc2634277255f95847
Marcel Walk <walk.marcel.97@googlemail.com>
2021-02-07 11:26:58 +0100

    Dotnet Console Base

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StatiqBlog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
