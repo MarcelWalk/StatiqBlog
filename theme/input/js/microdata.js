68e55e33fe74fa96f65ee04d32f5a9cdedc0d647
Marcel Walk <walk.marcel.97@googlemail.com>
2021-02-09 20:09:47 +0100

    Added microdata

---
+++ theme/input/js/microdata.js
@@ -0,0 +1,41 @@
+{
+    "@context": "http://schema.org/",
+    "@type": "CreativeWork",
+    "about": "Personal website and blog of Marcel Walk",
+    "creator": {
+        "@type": "Person",
+        "givenName": "Marcel",
+        "familyName": "Walk",
+        "gender": "Male",
+        "jobTitle": "Software Developer",
+        "birthDate": "1997-05-21",
+        "homeLocation":{
+            "@type": "Thing",
+            "address": "Leoben, Steiermark"
+        },
+        "knowsAbout": {
+            "@type": "Thing",
+            "name": "C#",
+            "description": "Programming language developed by Microsoft"
+        },
+        "url": "https://zealous-mcnulty-e9d3f7.netlify.app/"
+    },
+  "maintainer": {
+        "@type": "Person",
+        "givenName": "Marcel",
+        "familyName": "Walk",
+        "gender": "Male",
+        "jobTitle": "Software Developer",
+        "birthDate": "1997-05-21",
+        "homeLocation":{
+            "@type": "Thing",
+            "address": "Leoben, Steiermark"
+        },
+        "knowsAbout": {
+            "@type": "Thing",
+            "name": "C#",
+            "description": "Programming language developed by Microsoft"
+        },
+        "url": "https://zealous-mcnulty-e9d3f7.netlify.app/"
+    }
+}
